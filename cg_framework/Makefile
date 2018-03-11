66121921f23e7919f865841d852d0ac1098d8079
Gabriel <gabriel.leite083@gmail.com>
2018-03-11 10:51:29 -0300

    Arquivos iniciais

---
+++ cg_framework/Makefile
@@ -0,0 +1,8 @@
+CC = g++
+
+all:
+	$(CC) main.cpp -lglut -lGLU -lGL -I. -o cgprog
+
+clean:
+	rm cgprog
+
---
+++ cg_framework/main.cpp
@@ -0,0 +1,30 @@
+#include "main.h"
+
+//-----------------------------------------------------------------------------
+void MyGlDraw(void)
+{
+	//*************************************************************************
+	// Chame aqui as funções do mygl.h
+	//*************************************************************************
+
+
+
+}
+
+//-----------------------------------------------------------------------------
+int main(int argc, char **argv)
+{
+	// Inicializações.
+	InitOpenGL(&argc, argv);
+	InitCallBacks();
+	InitDataStructures();
+
+	// Ajusta a função que chama as funções do mygl.h
+	DrawFunc = MyGlDraw;	
+
+	// Framebuffer scan loop.
+	glutMainLoop();
+
+	return 0;
+}
+
